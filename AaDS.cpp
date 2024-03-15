9fd573828c5066fd0bd9022de661c9ca60d1d2ec
Roman <123767751+roma6kk@users.noreply.github.com>
2024-03-15 15:25:58 +0300

    Add files via upload

---
+++ AaDS.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<ctime>
+using namespace std;
+unsigned int fib(int n) {
+	if (n == 1)
+		return 0;
+	if (n == 2)
+		return 1;
+	return fib(n - 2) + fib(n - 1);
+}
+int main()
+{
+	setlocale(0, "rus");
+	int n;
+	cout << "Введите число N: " << endl;
+	cin >> n;
+	unsigned int* fibb = new unsigned int[n];
+	fibb[0] = 0;
+	fibb[1] = 1;
+	unsigned int start_time = clock();
+	for (int i = 2; i < n; i++)
+	{
+		fibb[i] = fibb[i - 2] + fibb[i - 1];
+	}
+	cout << "Число Фиббаначи с номером N через цикл: " << fibb[n-1] << endl;
+	unsigned int end_time = clock();
+	unsigned int search_time = end_time - start_time;
+	cout << "Расчетное время циклом " << search_time / 1000 / 60 << " минут " << search_time / 1000 % 60 << " секунд." << endl;
+	unsigned int start_time2 = clock();
+	cout << "Число Фиббаначи с номером N через рекурсию: " << fib(n) << endl;
+	unsigned int end_time2 = clock();
+	unsigned int search_time2 = end_time2 - start_time2;
+	cout << "Расчетное время рекурсией " << search_time2 / 1000 / 60 << " минут " << search_time2 / 1000 % 60 << " секунд." << endl;
+
+} 
---
+++ AaDS2.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	setlocale(0, "rus");
+	int N, X, move;
+	cout << "Введите число N: " << endl;
+	cin >> N;
+	if (N < 1)
+	{
+		cerr << "Число N не может быть меньше 1!" << endl;
+		return 1;
+	}
+	if (N == 1)
+	{
+		cout << "Ваше число - 1!" << endl;
+		return 0;
+	}
+	int  max_steps = ceil(log2(N));
+	int max = N + 1, min = 0;
+	cout << "Максимальное количество шагов в игре: " << max_steps <<endl;
+	while(min != max)
+	{
+		X = (min + max) / 2;
+		if (X == min || X == max)
+		{
+			cerr << "Ошибка со стороны пользователя!" << endl;
+			return 1;
+		}
+		cout << "Число - " << X << "?" << endl;
+		cout << "1 - мало, 2 - много 3 - угадал. " << endl;
+		cin >> move;
+		switch (move)
+		{
+		case 1:
+		{
+			min = X;
+			break;
+		}
+		case 2:
+		{
+			max = X;
+			break;
+		}
+		case 3:
+		{
+			cout << "Ваше число - " << X << "." << "Спасибо за игру." << endl;
+			return 0;
+		}
+		default: 
+			cout << "Ошибка ввода! " << endl;
+			break;
+		}
+	}
+	cout << "Ошибка со стороны пользователя" << endl;
+	return 1;
+
+}
